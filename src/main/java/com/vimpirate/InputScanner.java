4038ed21c3dd37ad163cd869c06bac1c9c910243
David Paul Graham <dpgraham4401@gmail.com>
2024-08-21 16:55:06 -0500

    example from book using Scanner class

---
+++ src/main/java/com/vimpirate/InputScanner.java
@@ -0,0 +1,16 @@
+package com.vimpirate;
+
+import java.util.Scanner;
+
+public class InputScanner {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int i = input.nextInt();
+        double d = input.nextDouble();
+
+        System.out.println("Integer: " + i);
+        System.out.println("Double: " + d);
+    }
+}
