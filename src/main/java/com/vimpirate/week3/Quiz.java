8f21e82df06d01aba2cd310aa15c31696a7955eb
David Paul Graham <dpgraham4401@gmail.com>
2024-09-02 08:25:26 -0400

    quiz 3.14 question 5

--- src/main/java/com/vimpirate/week3/Quiz.java
+++ src/main/java/com/vimpirate/week3/Quiz.java
@@ -3,10 +3,11 @@ package com.vimpirate.week3;
 public class Quiz {
 
     public static void main(String[] args) {
-        int x, y;
-        x = 0;
-        y = (x > 0) ? 10 : -10;
-        System.out.println(y);
+        boolean even;
+        int number = 3;
+        even = number % 2 == 0;
+
+        System.out.println(even);
 
     }
 }
