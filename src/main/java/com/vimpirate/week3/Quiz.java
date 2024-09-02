8f332b0bb62ef66d275ac876a5b72db5b2572386
David Paul Graham <dpgraham4401@gmail.com>
2024-09-01 21:13:20 -0400

    quiz 3.??

--- src/main/java/com/vimpirate/week3/Quiz.java
+++ src/main/java/com/vimpirate/week3/Quiz.java
@@ -5,20 +5,10 @@ import java.util.Scanner;
 public class Quiz {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int grade = scanner.nextInt();
-        String result = "";
-        if (grade >= 90) {
-            result = "Your grade is: A";
-        } else if (grade >= 80) {
-            result = "Your grade is: B";
-        } else if (grade >= 70) {
-            result = "Your grade is: C";
-        } else if (grade >= 60) {
-            result = "Your grade is: D";
-        } else {
-            result = "Your grade is: F";
+        boolean even = false;
+        if (even = true) {
+            System.out.println("It is even");
         }
-        System.out.println(result);
+
     }
 }
