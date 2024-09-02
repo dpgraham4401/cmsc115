b37c8ab5a1d73c7b6162086377ef2624e759de98
David Paul Graham <dpgraham4401@gmail.com>
2024-09-02 08:23:29 -0400

    quiz 3.14 question 4

--- src/main/java/com/vimpirate/week3/Quiz.java
+++ src/main/java/com/vimpirate/week3/Quiz.java
@@ -3,12 +3,10 @@ package com.vimpirate.week3;
 public class Quiz {
 
     public static void main(String[] args) {
-        int age = 25; // Example age
-        String category = (age >= 0 && age <= 12) ? "Child" :
-                (age >= 13 && age <= 19) ? "Teen" :
-                        (age >= 20 && age <= 64) ? "Adult" :
-                                "Senior";
-        System.out.println(category);
+        int x, y;
+        x = 0;
+        y = (x > 0) ? 10 : -10;
+        System.out.println(y);
 
     }
 }
