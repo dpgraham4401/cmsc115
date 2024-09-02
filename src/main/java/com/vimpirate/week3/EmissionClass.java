603a2754823f831f6b00074f1f5d8dea97552867
David Paul Graham <dpgraham4401@gmail.com>
2024-09-02 08:21:19 -0400

    quiz 3.14 question 2

--- src/main/java/com/vimpirate/week3/EmissionClass.java
+++ src/main/java/com/vimpirate/week3/EmissionClass.java
@@ -22,16 +22,5 @@ public class EmissionClass {
                 break;
         }
 
-        int x;
-        double d = 1.5;
-
-        switch (d) {
-            case 1.0:
-                x = 1;
-            case 1.5:
-                x = 2;
-            case 2.0:
-                x = 3;
-        }
     }
 }
--- src/main/java/com/vimpirate/week3/Quiz.java
+++ src/main/java/com/vimpirate/week3/Quiz.java
@@ -1,14 +1,14 @@
 package com.vimpirate.week3;
 
-import java.util.Scanner;
-
 public class Quiz {
 
     public static void main(String[] args) {
-        boolean even = false;
-        if (even = true) {
-            System.out.println("It is even");
-        }
+        int age = 25; // Example age
+        String category = (age >= 0 && age <= 12) ? "Child" :
+                (age >= 13 && age <= 19) ? "Teen" :
+                        (age >= 20 && age <= 64) ? "Adult" :
+                                "Senior";
+        System.out.println(category);
 
     }
 }
