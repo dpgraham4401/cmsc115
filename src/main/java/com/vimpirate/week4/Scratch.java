109e85f41989e143df845de4cf3fda6015147b68
David Paul Graham <dpgraham4401@gmail.com>
2024-09-05 20:59:15 -0400

    quiz 4.3.3

--- src/main/java/com/vimpirate/week4/Scratch.java
+++ src/main/java/com/vimpirate/week4/Scratch.java
@@ -2,15 +2,8 @@ package com.vimpirate.week4;
 
 public class Scratch {
     public static void main(String[] args) {
-        double x = 10.0;
-        double result;
-        if (x > 0) {
-            result = x * Math.log(x) - ((double) 3 / 2) * x;
-        } else if (x < 0) {
-            result = 2 - (3 / x) + (3 / Math.pow(x, 2));
-        } else {
-            result = Double.POSITIVE_INFINITY;
-        }
-        System.out.println(result);
+        char c1 = 'a';
+        char c2 = 'b';
+        System.out.println((Character.isAlphabetic(c1)));
     }
 }
