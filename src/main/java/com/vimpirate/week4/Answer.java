ad4fb776b6cde22b0095558353cba13a079cbfae
David Paul Graham <dpgraham4401@gmail.com>
2024-09-06 19:35:01 -0400

    quiz question 4.4.3 question 3

--- src/main/java/com/vimpirate/week4/Answer.java
+++ src/main/java/com/vimpirate/week4/Answer.java
@@ -2,11 +2,7 @@ package com.vimpirate.week4;
 
 public class Answer {
     public static void main(String[] args) {
-        String siteAddress = "www.example.com";
-        String resourcePath = "contact";
-        boolean isSecure = false;
-
-        String url = (isSecure ? "https://" : "http://") + siteAddress + (resourcePath.startsWith("/") ? resourcePath : "/" + resourcePath);
-        System.out.println(url);
+        String hello = "Java " + 1 + 2 + 3;
+        System.out.println(hello);
     }
 }
