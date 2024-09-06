409b308792f05e7a90299ddc893155ff39d3f105
David Paul Graham <dpgraham4401@gmail.com>
2024-09-06 19:41:56 -0400

    quiz question 4.4.3 question 4

--- src/main/java/com/vimpirate/week4/Answer.java
+++ src/main/java/com/vimpirate/week4/Answer.java
@@ -2,7 +2,7 @@ package com.vimpirate.week4;
 
 public class Answer {
     public static void main(String[] args) {
-        String hello = "Java " + 1 + 2 + 3;
+        char hello = 'A' + 1;
         System.out.println(hello);
     }
 }
