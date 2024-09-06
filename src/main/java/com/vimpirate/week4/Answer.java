f6a01af0ba17b4d8a5988d68aaade40cbd8717e1
David Paul Graham <dpgraham4401@gmail.com>
2024-09-06 19:32:38 -0400

    quiz question 4.4.2

--- src/main/java/com/vimpirate/week4/Answer.java
+++ src/main/java/com/vimpirate/week4/Answer.java
@@ -1,17 +1,11 @@
 package com.vimpirate.week4;
 
-import java.util.Scanner;
-
 public class Answer {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        String formattedMessage;
+        String message = "Hello World!";
 
-        int input = scanner.nextInt();
-
-        if (input >= 0 && input <= 127) {
-            System.out.println("ASCII - '" + (char) input + "'");
-        } else {
-            System.out.println("Unicode - '" + (char) input + "'");
-        }
+        formattedMessage = message.length() + " " + message;
+        System.out.println(formattedMessage);
     }
 }
