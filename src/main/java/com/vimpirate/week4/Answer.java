b1b12922bf716acf065487f6315e8feebaaed158
David Paul Graham <dpgraham4401@gmail.com>
2024-09-06 19:55:15 -0400

    quiz 4.4.7 question 3

--- src/main/java/com/vimpirate/week4/Answer.java
+++ src/main/java/com/vimpirate/week4/Answer.java
@@ -2,7 +2,15 @@ package com.vimpirate.week4;
 
 public class Answer {
     public static void main(String[] args) {
-        char hello = 'A' + 1;
-        System.out.println(hello);
+        String previousTitle = "hello";
+        String title = "foo";
+        if (title.compareToIgnoreCase(previousTitle) < 0) {
+            System.out.println("\"" + title + "\" comes before \"" + previousTitle + "\"");
+        } else if (title.compareToIgnoreCase(previousTitle) > 0) {
+            System.out.println("\"" + title + "\" comes after \"" + previousTitle + "\"");
+        } else {
+            System.out.println("\"" + title + "\" is equal to \"" + previousTitle + "\"");
+        }
+
     }
 }
