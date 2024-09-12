13440a4d829483dce8a6669e1e9d73f8010a02e4
David Paul Graham <dpgraham4401@gmail.com>
2024-09-12 19:48:13 -0400

    quiz 5.3 question 2

--- src/main/java/com/vimpirate/week5/Scratch.java
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -1,5 +1,7 @@
 package com.vimpirate.week5;
 
+import java.util.Scanner;
+
 /**
  * class I use for testing code snippets
  *
@@ -8,12 +10,21 @@ package com.vimpirate.week5;
 public class Scratch {
 
     public static void main(String[] args) {
-        java.util.Scanner stdin = new java.util.Scanner(System.in);
-        String username = "";
-        int minLength = 8;
-        while (username.length() < minLength) {
-            username = stdin.next();
+        Scanner stdin = new Scanner(System.in);
+        int count = 0;
+        int previousReading = stdin.nextInt();
+
+        while (true) {
+            int currentReading = stdin.nextInt();
+            if (currentReading < 0) {
+                break;
+            }
+            if (Math.abs(currentReading - previousReading) > 5) {
+                count++;
+            }
+            previousReading = currentReading;
         }
-        System.out.println(username);
+
+        System.out.println(count);
     }
 }
