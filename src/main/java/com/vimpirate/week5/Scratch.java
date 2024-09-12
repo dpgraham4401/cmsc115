c57cec153877d921ac3cde7688bb36d004d42669
David Paul Graham <dpgraham4401@gmail.com>
2024-09-12 19:02:49 -0400

    quiz 5.2 question 1

---
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -0,0 +1,20 @@
+package com.vimpirate.week5;
+
+/**
+ * class I use for testing code snippets
+ *
+ * @Author: David Graham
+ */
+public class Scratch {
+
+    public static void main(String[] args) {
+        int level = 1;
+        int hp = 1;
+
+        while (level < 10) {
+            hp *= level;
+            level++;
+        }
+
+    }
+}
