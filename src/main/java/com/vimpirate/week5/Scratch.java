7385bf38ea2493d18edaf4c02cb92539a453acba
David Paul Graham <dpgraham4401@gmail.com>
2024-09-12 19:41:38 -0400

    quiz 5.3 question 1

--- src/main/java/com/vimpirate/week5/Scratch.java
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -9,10 +9,11 @@ public class Scratch {
 
     public static void main(String[] args) {
         java.util.Scanner stdin = new java.util.Scanner(System.in);
-        int userInput = 1;
-        while (userInput % 3 != 0) {
-            System.out.println("Enter a number divisible by 3: ");
-            userInput = stdin.nextInt();
+        String username = "";
+        int minLength = 8;
+        while (username.length() < minLength) {
+            username = stdin.next();
         }
+        System.out.println(username);
     }
 }
