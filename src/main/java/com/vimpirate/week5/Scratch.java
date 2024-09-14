8eedd84a6ecfcc74a0eb5fa61898d90f402f5a6c
David Paul Graham <dpgraham4401@gmail.com>
2024-09-14 10:46:19 -0400

    quiz 5.9 question 3

--- src/main/java/com/vimpirate/week5/Scratch.java
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -1,7 +1,5 @@
 package com.vimpirate.week5;
 
-import java.util.Scanner;
-
 /**
  * class I use for testing code snippets
  *
@@ -10,23 +8,13 @@ import java.util.Scanner;
 public class Scratch {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        // prompt user for the value of n
-        System.out.print("Enter the value of n: ");
-        int n = input.nextInt();
-
-        for (int i = 1; i <= n; i++) {
-            // print leading spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+        int count = 0;
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < i; j++) {
+                count++;
+                System.out.println(count);
             }
-            // print numbers
-            for (int k = 1; k <= i; k++) {
-                System.out.print(i);
-            }
-            System.out.println();
-        }
+
     }
 
 }
