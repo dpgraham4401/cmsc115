9623f997495a497f3f22c1dff4909e2c41e70735
David Paul Graham <dpgraham4401@gmail.com>
2024-09-14 08:59:19 -0400

    quiz 5.7 question 5

--- src/main/java/com/vimpirate/week5/Scratch.java
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -1,5 +1,7 @@
 package com.vimpirate.week5;
 
+import java.util.Scanner;
+
 /**
  * class I use for testing code snippets
  *
@@ -8,11 +10,25 @@ package com.vimpirate.week5;
 public class Scratch {
 
     public static void main(String[] args) {
-        for (int i = 300; i >= 150; i--) {
-            if (i % 3 == 0) {
-                System.out.printf("%d ", i);
+        Scanner stdin = new Scanner(System.in);
+
+        // Take two integer inputs from the user
+        System.out.print("Enter the first number: ");
+        int num1 = stdin.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = stdin.nextInt();
+
+        int gcd = 1; // Initialize GCD to 1
+
+        // Use a for loop to find the GCD
+        for (int i = 1; i <= Math.min(num1, num2); i++) {
+            if (num1 % i == 0 && num2 % i == 0) {
+                gcd = i; // Update GCD
             }
         }
+
+        // Print the result
+        System.out.println("The Greatest Common Denominator is: " + gcd);
 
     }
 
