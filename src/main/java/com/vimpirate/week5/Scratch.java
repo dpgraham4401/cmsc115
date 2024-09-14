522627436830a3788f1d6544dfe022aa30eacf14
David Paul Graham <dpgraham4401@gmail.com>
2024-09-14 10:47:54 -0400

    quiz 5.10 question 1

--- src/main/java/com/vimpirate/week5/Scratch.java
+++ src/main/java/com/vimpirate/week5/Scratch.java
@@ -8,13 +8,11 @@ package com.vimpirate.week5;
 public class Scratch {
 
     public static void main(String[] args) {
-        int count = 0;
-        for (int i = 0; i < 10; i++)
-            for (int j = 0; j < i; j++) {
-                count++;
-                System.out.println(count);
-            }
-
+        int y = 0;
+        for (int i = 0; i < 10; ++i) {
+            y += 1;
+        }
+        System.out.println(y);
     }
 
 }
